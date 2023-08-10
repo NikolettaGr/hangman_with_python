d1ac3ca8af61c93b03f0170e5982f625f20af023
nikoletta <grishaberN@outlook.com>
2023-08-10 08:47:08 +0200

    Add clear_screen function for terminal screen clearing

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 import random
 import os
 from words import easy_list, medium_list, hard_list
+
+def clear_screen():
+    """
+    Clears the terminal screen based on
+    the operating system type.
+    """
+    os.system("cls" if os.name == "nt" else "clear")
