fa9602370bdfd88d7bbf2b924f541934ccbdea00
nikoletta <grishaberN@outlook.com>
2023-08-10 08:39:48 +0200

    Import random and os modules, and words from external module

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import os
+from words import easy_list, medium_list, hard_list
