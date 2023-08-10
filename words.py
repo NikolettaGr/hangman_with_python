ce7bf8498f1a71ae054053c3e24fbb2f3db48c5f
nikoletta <grishaberN@outlook.com>
2023-08-10 08:11:27 +0200

    Add word list to the game

---
+++ words.py
@@ -0,0 +1,38 @@
+easy_list = [
+    'pear',
+    'apple',
+    'lime',
+    'corn',
+    'bean',
+    'beet',
+    'kiwi',
+    'plum',
+    'kale',
+    'leek'
+]
+
+medium_list = [
+    'jacuzzi',
+    'cabbage',
+    'raceway',
+    'couple',
+    'artist',
+    'bamboo',
+    'camera',
+    'desert',
+    'believe',
+    'gorilla'
+]
+
+hard_list = [
+    'apocrypha',
+    'blackmail',
+    'clickable',
+    'flashback',
+    'milkshake',
+    'background',
+    'earthquake',
+    'hypnotized',
+    'australia',
+    'margaritas'
+]
